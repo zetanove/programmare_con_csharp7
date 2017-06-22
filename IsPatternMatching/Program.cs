4dd0e0b0e043184b02196a321a188dad4dd3d5d1
zetanove <antonio.pelleriti@gmail.com>
2017-06-22 10:18:17 +0200

    pattern matching con operatore is

---
+++ IsPatternMatching/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsPatternMatching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*TYPE PATTERN*/
+            object obj = "Hello, world";
+
+            if (obj is string str)
+                Console.WriteLine($"lunghezza {str.Length}");
+
+            //equivalente a 
+            if (obj is string)
+            {
+                string s = (string)obj;
+                Console.WriteLine($"lunghezza {s.Length}");
+            }
+        }
+    }
+}
