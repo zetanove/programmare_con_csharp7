ee9ee46dc1cab91926edd57f14413ffe1534b488
zetanove <antonio.pelleriti@gmail.com>
2017-06-29 06:53:56 +0200

    tuple native

---
+++ TupleLiteral/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TupleLiteral
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (int, string) tupla = (1, "Matilda"); //Tuple.Create<int, string>(1, "");
+
+            Coordinate coord = new Coordinate("Roma", 41.891930, 12.5113300);
+
+            var (città, lat, lon) = coord;
+        }
+    }
+
+    public class Coordinate
+    {
+        public string Città { get; }
+        public double Latitudine { get; }
+        public double Longitudine { get; }
+
+        public Coordinate(string città, double lat, double lon)
+        {
+            Città = città;
+            Latitudine = lat;
+            Longitudine = lon;
+        }
+
+        public void Deconstruct(out string città, out double latitudine, out double longitudine)
+        {
+            città = this.Città;
+            latitudine = this.Latitudine;
+            longitudine = this.Longitudine;
+        }
+    }
+
+}
